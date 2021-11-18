680bc65c162363a922ec57379bc44dda72c77bc7
Umi <umi.evans@ashlingpartners.com>
2021-11-18 19:13:35 +0000

    created file structure

--- models/example/table_list.sql
+++
@@ -1,4 +0,0 @@
---the tables that are used in this tutorial
-select * from `dbt-tutorial.stripe.payment`;
-select * from `dbt-tutorial.jaffle_shop.customers`;
-select * from `dbt-tutorial.jaffle_shop.orders`;
