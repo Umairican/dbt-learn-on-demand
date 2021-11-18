8469e4a454398565f49730d496b9b5820b1f8ee8
Umi <umi.evans@ashlingpartners.com>
2021-11-18 18:09:24 +0000

    created first model

---
+++ models/example/table_list.sql
@@ -0,0 +1,4 @@
+--the tables that are used in this tutorial
+select * from `dbt-tutorial.stripe.payment`;
+select * from `dbt-tutorial.jaffle_shop.customers`;
+select * from `dbt-tutorial.jaffle_shop.orders`;
